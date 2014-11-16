377956938eb044eec167980730128d6af6db0c23
ho <hataeho1@naver.com>
2014-11-17 02:52:42 +0900

    Move files

--- WebApp/module/pages/register/js/fail_Layout.js
+++ WebApp/module/pages/register/js/fail_Layout.js
@@ -1,9 +1,9 @@
 /* NHNNEXT Development Practice Project RUWORKING HTH 141116 */ 
 ;
 (function() {
-	// 회원가입 실패시 표시되는 안내 레이어가 5초후 사라지도록 조치
+	// 회원가입 실패시 표시되는 안내 레이어가 10초후 사라지도록 조치
     document.addEventListener("DOMContentLoaded", function() {
     		var registerFailEle = document.querySelector("span.fail");
-        setTimeout(function(){registerFailEle.style.display="none"}, 5000);
+        setTimeout(function(){registerFailEle.style.display="none"}, 10000);
     }, false);
 })();
