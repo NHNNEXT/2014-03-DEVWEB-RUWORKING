54deb559c84414492679518ebe25ac8641beae38
ho <hataeho1@naver.com>
2014-12-23 16:40:10 +0900

    #37 Connection Bug Fix

--- src/service/main/MainModel.java
+++ src/service/main/MainModel.java
@@ -14,42 +14,24 @@ public class MainModel {
 		ArrayList<Politician> Top5List = new ArrayList<Politician>();
 		try {
 			while(rs.next()){
-				int politicianId = rs.getInt("politician_id");
 				int percent = rs.getInt("rate");
-				Top5List.add(new Politician(rs.getInt("politician_id"), getPolitician(politicianId).getString("name"), getPolitician(politicianId).getString("local"), getParty(getPolitician(politicianId).getInt("party_id")), getPolitician(politicianId).getString("img_Url"), percent));
+				Top5List.add(new Politician(rs.getInt("id"), rs.getString("name"), rs.getString("local"), rs.getString("party_name"), rs.getString("img_Url"), percent));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		return Top5List;
-	}
-	
-	private String getParty(int partyId) {
-		ArrayList<Object> queryValues = new ArrayList<Object>();
-		String sql = "SELECT * from party where id = ?";
-		
-		queryValues.add(partyId);
-		PstmtQuerySet querySet = new PstmtQuerySet(sql, queryValues);
-		
-		DAOFactory DAO = new DAOFactory();
-		ResultSet rs = null;
-		try {
-			rs = DAO.selectQuery(querySet);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		try {
-			if(rs.next())
-				return rs.getString("name");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 
 	private ResultSet getFulfillment(int numOfPolitician){
 		ArrayList<Object> queryValues = new ArrayList<Object>();
-		String sql = "SELECT FLOOR(SUM(vote_score/vote_count)/5) AS rate, politician_id from promise GROUP BY politician_id ORDER BY SUM(vote_score/vote_count)/5 DESC LIMIT ?";
+		String sql = "SELECT FLOOR(SUM(vote_score/vote_count)/5) AS rate, politician.*, party.name AS party_name from promise inner join politician on promise.politician_id = politician.id inner join party on politician.party_id = party.id GROUP BY politician_id ORDER BY SUM(vote_score/vote_count)/5 DESC LIMIT ?";
 		
 		queryValues.add(numOfPolitician);
 		PstmtQuerySet querySet = new PstmtQuerySet(sql, queryValues);
@@ -61,30 +43,6 @@ public class MainModel {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
 		return rs;
-	}
-	
-	private ResultSet getPolitician(int politicianId){
-		ArrayList<Object> queryValues = new ArrayList<Object>();
-		String sql = "SELECT * from politician where id=?";
-		
-		queryValues.add(politicianId);
-		PstmtQuerySet querySet = new PstmtQuerySet(sql, queryValues);
-				
-		DAOFactory DAO = new DAOFactory();
-		ResultSet rs = null;
-		try {
-			rs = DAO.selectQuery(querySet);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		try {
-			if(rs.next())
-				return rs;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 }
