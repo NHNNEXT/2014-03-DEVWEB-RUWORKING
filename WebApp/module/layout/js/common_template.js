af4dfd5efde3f739e025db73b31f8b90d53b46c1
ho <hataeho1@naver.com>
2015-01-03 12:01:35 +0900

    Go Top

--- WebApp/module/layout/js/common_template.js
+++ WebApp/module/layout/js/common_template.js
@@ -28,10 +28,44 @@
         politicianEleOnNav.addEventListener("click", function(e){
             searchinfoEle.style.display="block";
             setNavOn(e);
+            gotop();
         }, false);
         setNavMenu();
     }, false);
 
+    function gotop(orix,oriy,desx,desy) {
+        var Timer;
+        if (document.body.scrollTop == 0) {
+            var winHeight = document.documentElement.scrollTop;
+        } else {
+            var winHeight = document.body.scrollTop;
+        }
+        if(Timer) clearTimeout(Timer);
+        startx = 0;
+        starty = winHeight;
+        if(!orix || orix < 0) orix = 0;
+        if(!oriy || oriy < 0) oriy = 0;
+        var speed = 7;
+
+        if(!desx) desx = 0 + startx;
+        if(!desy) desy = 0 + starty;
+        desx += (orix - startx) / speed;
+        if (desx < 0) desx = 0;
+        desy += (oriy - starty) / speed;
+        if (desy < 0) desy = 0;
+        var posX = Math.ceil(desx);
+        var posY = Math.ceil(desy);
+        window.scrollTo(posX, posY);
+        if((Math.floor(Math.abs(startx - orix)) < 1) && (Math.floor(Math.abs(starty - oriy)) < 1)){
+            clearTimeout(Timer);
+            window.scroll(orix,oriy);
+        }else if(posX != orix || posY != oriy){
+            Timer = setTimeout(function(){gotop(orix,oriy,desx,desy);},15);
+        }else{
+            clearTimeout(Timer);
+        }
+    }
+    
     var originText;
     function setNavOn(e) {
         var originEle = e.target.parentElement.querySelector(".on");
